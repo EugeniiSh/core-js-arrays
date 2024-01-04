c13bb26904affad792fdbfbbf1fe746a636c5d01
EugeniiSh <evgenii.shmerko@mail.ru>
2024-01-04 14:50:44 +0300

    feat: add 6 functions

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,15 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  // return Array(end - start + 1)
+  //   .fill(0)
+  //   .map((item, index) => start + index);
+
+  return Array.from(
+    { length: end - start + 1 },
+    (item, index) => start + index
+  );
 }
 
 /**
@@ -37,8 +44,25 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let minLength;
+  let maxArr;
+  let minArr;
+  if (arr1.length === arr2.length) {
+    maxArr = arr1;
+    minArr = arr2;
+    minLength = minArr.length;
+  } else {
+    maxArr = arr1.length > arr2.length ? arr1 : arr2;
+    minArr = arr1.length < arr2.length ? arr1 : arr2;
+    minLength = minArr.length;
+  }
+
+  for (let i = 0; i < minLength; i += 1) {
+    maxArr[i] += minArr[i];
+  }
+
+  return maxArr;
 }
 
 /**
@@ -53,8 +77,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -71,8 +95,8 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((value) => value === item).length;
 }
 
 /**
@@ -87,8 +111,9 @@ function findAllOccurrences(/* arr, item */) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter((item) => Boolean(item) === true);
+  // return arr.filter((item) => item);
 }
 
 /**
@@ -101,8 +126,8 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  return arr.map((item) => item.length);
 }
 
 /**
@@ -119,8 +144,19 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  let result = 0;
+  if (arr.length === 0) {
+    return result;
+  }
+  // for (let i = 0; i < arr.length; i += 1) {
+  //   result += arr[i];
+  // }
+  // return Math.round((result / arr.length) * 100) / 100;
+  result =
+    Math.round((arr.reduce((sum, item) => sum + item, 0) / arr.length) * 100) /
+    100;
+  return result;
 }
 
 /**
@@ -133,8 +169,20 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  // for (let i = 1; i < arr.length; i += 1) {
+  //   if (arr[0].length !== arr[i].length) {
+  //     return false;
+  //   }
+  // }
+  // return true;
+  // let result = null;
+  // result = arr.filter((item) => item.length === arr[0].length);
+  // if (result.length === arr.length) {
+  //   return true;
+  // }
+  // return false;
+  return arr.every((item) => item.length === arr[0].length);
 }
 
 /**
